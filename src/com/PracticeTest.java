c036f58067ab6e2c3503229d1812ad8b44733204
manishabhosale35 <manishabhosale35@gmail.com>
2025-02-11 01:27:17 +0530

    122

--- src/com/PracticeTest.java
+++ src/com/PracticeTest.java
@@ -13,5 +13,21 @@ public class PracticeTest {
 			int num2 = sc.nextInt();
 			System.out.println("Enter your third number");
 			int num3 = sc.nextInt();
-						
-	}}
+			int max = getMaxNumber(num1, num2, num3);
+			System.out.println("MaxNumber Number is :" + max);
+
+			
+		}
+
+		public static int getMaxNumber(int a, int b, int c) { // for maximum number calculation
+			int max = a;
+			if (b > max) {
+				max = b;
+			}
+			if (c > max) {
+				max = c;
+			}
+			return max;
+		}
+
+	}
