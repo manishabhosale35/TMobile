10bb3049ab0d6526e98394ca8bf867b95c61d4d4
manishabhosale35 <manishabhosale35@gmail.com>
2025-02-11 01:05:58 +0530

    new commit

---
+++ src/com/PracticeTest.java
@@ -0,0 +1,17 @@
+package com;
+
+import java.util.Scanner;
+
+public class PracticeTest {
+
+		public static void main(String[] args) {
+
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter your first number");
+			int num1 = sc.nextInt();
+			System.out.println("Enter your second number");
+			int num2 = sc.nextInt();
+			System.out.println("Enter your third number");
+			int num3 = sc.nextInt();
+						
+	}}
